c113f374adcd8db003501e59543e74187b8025f3
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-02-27 15:11:26 +0100

    Update Seperate plots.py

--- Seperate plots.py
+++ Seperate plots.py
@@ -25,17 +25,17 @@ labels = ["Trappist 1b", "Trappist 1c", "Trappist 1d", "Trappist 1e", "Trappist 
 
 
 
-
 # I nedestående loop plottes hvert datasæt et efter et
 
 for i in range(0,len(filenames)):
     Wavelength = np.loadtxt(filenames[i])[:, 0] # Datapunkternes bølgelængder læses fra filen
     Transmittance = np.loadtxt(filenames[i])[:, 1]*10**6 # Værdien af datapunkterne aflæses
-    
+    error = np.loadtxt(filenames[i])[:, 2]*10**6
     plt.figure()
+    
+    plt.errorbar(Wavelength, Transmittance, yerr=error, label='both limits (default)')
     plt.plot(Wavelength, Transmittance, colours[i], label = labels[i]) # Datasæt "i" plottes
     plt.title("Transmittance spektrum " + labels[i]) # plottets titel defineres
     plt.xlabel("wavelength [um]")                       # x-aksens titel defineres
     plt.ylabel("Transmittance [ppm]")                         # y-aksens titel defineres 
     plt.show
-
