0efbb01588b475f0977361e70bb1eb2ed5d7d705
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-02-27 10:28:19 +0100

    Update loadfiler.py

--- loadfiler.py
+++ loadfiler.py
@@ -2,9 +2,26 @@
 """
 Created on Mon Feb 20 13:54:18 2023
 
-@author: willi
+@author: William Hedegaard Langvad
 """
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
-file = np.loadtxt("psg_trn.txt")[:, 1]
-print(file)
+
+Rs = 0.12*696340
+Rp = 14012.4/2
+
+
+
+Wavelength = np.loadtxt("Trappist1C_O2.txt")[:, 0]
+signal = np.loadtxt("Trappist1C_O2.txt")[:, 1]
+
+
+Transmittance = signal/((Rp/Rs)**2)
+
+plt.plot(Wavelength, Transmittance)
+plt.title("Transmittance spectrum Trappist system") 
+plt.xlabel("wavelength [um]") 
+plt.ylabel("Transmittance")
+plt.show()
