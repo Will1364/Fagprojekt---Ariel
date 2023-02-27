1b309ea51ef06c24c96180241df58df9d30c474a
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-02-27 14:42:02 +0100

    Update loadfiler.py

--- loadfiler.py
+++ loadfiler.py
@@ -8,20 +8,26 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+# Her Skriver vi en liste over filnavne
+filenames = ["psg_rad_1b.txt","psg_rad_1c.txt", "psg_rad_1d.txt", "psg_rad_1e.txt", "psg_rad_1f.txt"]
 
-Rs = 0.12*696340
-Rp = 14012.4/2
+# Her definerer vi en liste med navne på hvert datasæt til vores plot
+#labels = ["1 bar H20 (N=50)", "1 bar H20 cloudy (N=90)", "10 bar CO2 (N=30)", "10 bar Venus (N=120)"]
+labels = ["Trappist 1b", "Trappist 1c", "Trappist 1d", "Trappist 1e", "Trappist 1f"]
 
+# Her beskrives de farver som bruges i plottet
+colours = ["r","b","g","y","c","m","k"]
 
+# I nedestående loop plottes hvert datasæt et efter et
+plt.figure()
+for i in range(0,len(filenames)):
+    Wavelength = np.loadtxt(filenames[i])[:, 0] # Datapunkternes bølgelængder læses fra filen
+    Transmittance = np.loadtxt(filenames[i])[:, 1]*10**6 # Værdien af datapunkterne aflæses
+    plt.plot(Wavelength, Transmittance, colours[i], label = labels[i]) # Datasæt "i" plottes
 
-Wavelength = np.loadtxt("Trappist1C_O2.txt")[:, 0]
-signal = np.loadtxt("Trappist1C_O2.txt")[:, 1]
-
-
-Transmittance = signal/((Rp/Rs)**2)
-
-plt.plot(Wavelength, Transmittance)
-plt.title("Transmittance spectrum Trappist system") 
-plt.xlabel("wavelength [um]") 
-plt.ylabel("Transmittance")
-plt.show()
+plt.title("Transmittance spectrum Trappist system") # plottets titel defineres
+plt.xlabel("wavelength [um]")                       # x-aksens titel defineres
+plt.ylabel("Transmittance")                         # y-aksens titel defineres
+plt.legend()                            # legend med titel 
+plt.show
+ 
