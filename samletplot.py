1338fc0747b47744aa7297686e6cb53628ce0873
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-02-27 14:59:17 +0100

    Update and rename loadfiler.py to samletplot.py

--- samletplot.py
+++ samletplot.py
@@ -7,16 +7,23 @@ Created on Mon Feb 20 13:54:18 2023
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+# Her beskrives de farver som bruges i plottet
+colours = ["ro","bo","go","yo","co","mo","ko"]
+
+
+###############################################################################
+#CO2 baserede atmosfærer
+###############################################################################
 
 # Her Skriver vi en liste over filnavne
-filenames = ["psg_rad_1b.txt","psg_rad_1c.txt", "psg_rad_1d.txt", "psg_rad_1e.txt", "psg_rad_1f.txt"]
+filenames = ["psg_rad_1b.txt","psg_rad_1c.txt", "psg_rad_1e.txt"]
 
 # Her definerer vi en liste med navne på hvert datasæt til vores plot
 #labels = ["1 bar H20 (N=50)", "1 bar H20 cloudy (N=90)", "10 bar CO2 (N=30)", "10 bar Venus (N=120)"]
 labels = ["Trappist 1b", "Trappist 1c", "Trappist 1d", "Trappist 1e", "Trappist 1f"]
 
-# Her beskrives de farver som bruges i plottet
-colours = ["r","b","g","y","c","m","k"]
+
+
 
 # I nedestående loop plottes hvert datasæt et efter et
 plt.figure()
@@ -25,9 +32,8 @@ for i in range(0,len(filenames)):
     Transmittance = np.loadtxt(filenames[i])[:, 1]*10**6 # Værdien af datapunkterne aflæses
     plt.plot(Wavelength, Transmittance, colours[i], label = labels[i]) # Datasæt "i" plottes
 
-plt.title("Transmittance spectrum Trappist system") # plottets titel defineres
+plt.title("Transmittance spektrum CO2 baseret atmosfærer") # plottets titel defineres
 plt.xlabel("wavelength [um]")                       # x-aksens titel defineres
-plt.ylabel("Transmittance")                         # y-aksens titel defineres
+plt.ylabel("Transmittance [ppm]")                         # y-aksens titel defineres
 plt.legend()                            # legend med titel 
 plt.show
- 
