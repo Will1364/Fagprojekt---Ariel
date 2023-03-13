7ee70bbcffa6b7996ab4e29b98ec86de3dd25abc
Tennisbolden123 <110529292+Tennisbolden123@users.noreply.github.com>
2023-03-13 14:31:11 +0100

    Add files via upload

---
+++ BIC_funktion.py
@@ -0,0 +1,12 @@
+# Author CVL
+# DeltaBIC funktion
+# Input: chi^2-fordelingen for henholdsvis den fittede model og for 
+# en flad linje og n (antallet af datapunkter).
+# Output: DeltaBIC (hvor godt dataen passser modellen ift. en flad linje)
+def BIC(chi_anden_m,chi_anden_f,n)
+    k = 20
+    BIC_m = chi_anden_m + k*ln(n)
+    BIC_f = chi_anden_f + k*ln(n)
+    DeltaBIC = chi_anden_m - chi_anden_f
+    return DeltaBIC
+
