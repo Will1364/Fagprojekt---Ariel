1d57a20a68d66557730301192839b420b27c7350
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-03-17 12:00:30 +0100

    Update BIC_funktion.py

--- BIC_funktion.py
+++ BIC_funktion.py
@@ -3,7 +3,7 @@
 # Input: chi^2-fordelingen for henholdsvis den fittede model og for 
 # en flad linje og n (antallet af datapunkter).
 # Output: DeltaBIC (hvor godt dataen passser modellen ift. en flad linje)
-def BIC(chi_anden_m,chi_anden_f,n)
+def BIC(chi_anden_m,chi_anden_f,n):
     k = 20
     BIC_m = chi_anden_m + k*ln(n)
     BIC_f = chi_anden_f + k*ln(n)
