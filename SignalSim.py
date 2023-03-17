7a5070a0928bfb1d2ed5e75a4632f31ea8e8a680
ThildeS <126470153+ThildeS@users.noreply.github.com>
2023-03-17 11:45:25 +0100

    Add files via upload
    Signal func

---
+++ SignalSim.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 10:09:38 2023
+
+@author: Mathilde Saltoft 
+"""
+
+##############################################################################
+## Author : MAthilde Saltoft Schou
+
+# packeges for calc and script
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# The function for adding randomrumbers to the data points
+
+# define array of data points
+Wavelength = np.loadtxt("TRAPPIST1b.txt")[:, 0]
+Spectrum = np.loadtxt("TRAPPIST1b.txt")[:, 1]*10**6
+Noise = np.loadtxt("TRAPPIST1b.txt")[:, 2]*10**6
+
+
+def SignalSimulater(Spectrum, Noise):
+    n = len(Spectrum)
+    #np.random.seed(69314)     # Seed to check code
+    
+    RNDvector = np.array([])
+    
+    for i in range(0, n):
+        RND = np.random.normal(0, Noise[i],1)   # Generate Random nomber (RND) from norm dist with s = noise
+        print(RND)
+        RNDvector = np.append(RNDvector,RND)    # Create vector from RND
+    
+    
+    Signal = Spectrum + RNDvector       #Calculate Signal by adding random numbers to spectrum
+    return Signal
+
+Signal = SignalSimulater(Spectrum, Noise)
+
+plt.figure()
+plt.plot(Wavelength, Signal, "bo")
+plt.plot(Wavelength, Spectrum, "r")
+plt.show()
