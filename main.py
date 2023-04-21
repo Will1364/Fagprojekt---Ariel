ee80b197b9ff39252dd82f8eaa404952a69aad87
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-04-21 08:56:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,7 @@ for i in range(0,len(planets)):
     Wavelength = np.loadtxt("psg_" + planets[i] + "_c.txt")[:, 0] # Datapunkternes bølgelængder læses fra filen
     Spectrum = np.loadtxt("psg_" + planets[i] + "_c.txt")[:, 1]*10**6 # Værdien af datapunkterne aflæses
     error = np.loadtxt("psg_" + planets[i] + "_c.txt")[:, 2]*10**6
-    flatModel = np.loadtxt("psg_" + planets[i] + "_f.txt")[:, 1]*10**6
-
-    
-    diff = np.mean(Spectrum)-np.mean(flatModel)
-    flatModel  = flatModel+diff
-
+    flatModel = np.mean(Spectrum)
     
     avgBIC = np.array([])                                 #Dette stykke kode kan udregne en gennemsnitlig dBIC-værdi for 1000 simuleringer
     for j in range(1000):
@@ -63,7 +58,6 @@ for i in range(0,len(planets)):
     plt.errorbar(binnedData[1], binnedData[0],fmt=' ', yerr=binnedData[2], label='both limits (default)')
     plt.plot(binnedData[1], binnedData[0], "b.", label = planets[i]) # Datasæt "i" plottes
     plt.plot(Wavelength, Spectrum, colours[i])
-    plt.plot(Wavelength, flatModel,"r")
     plt.title("Transmittance spektrum " + planets[i]) # plottets titel defineres
     plt.xlabel("wavelength [um]")                     # x-aksens titel defineres
     plt.ylabel("Transmittance [ppm]")                 # y-aksens titel defineres 
