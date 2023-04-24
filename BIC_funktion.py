1192e43a7ef34234e496dd32148880646dd13cbb
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-04-24 11:40:39 +0200

    Update BIC_funktion.py

--- BIC_funktion.py
+++ BIC_funktion.py
@@ -4,12 +4,12 @@
 # en flad linje og n (antallet af datapunkter).
 # Output: DeltaBIC (hvor godt dataen passser modellen ift. en flad linje)
 import math
-import numpy as np
 
 def BIC(chi_anden_m,chi_anden_f,n):
     k = 20
     BIC_m = chi_anden_m + k*math.log(n)
     BIC_f = chi_anden_f + k*math.log(n)
-    DeltaBIC = BIC_m - BIC_f
+    
+    DeltaBIC = BIC_f-BIC_m
     return DeltaBIC
 
