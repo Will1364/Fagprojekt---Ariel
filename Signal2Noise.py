7dda9b8f7c225efb562d0d36606c55cc533e6450
ThildeS <126470153+ThildeS@users.noreply.github.com>
2023-06-02 11:30:31 +0200

    Update Signal2Noise.py

--- Signal2Noise.py
+++ Signal2Noise.py
@@ -8,9 +8,9 @@ This is a temporary script file.
 
 import math
 
-def chiSquared(Signal,Model,flatModel,Uncertainty):
+def SignalToNoise(Signal,Model,flatModel,Uncertainty):
     
-    SN_m = sum((Signal-Model)/Uncertainty)       #chi i anden udregnes for spektret
-    SN_f = sum((Signal-flatModel)/Uncertainty)   #chi i anden udregnes for den flade model
+    SN_m = sum((Signal-Model)/Uncertainty)       # S/N for spektret
+    SN_f = sum((Signal-flatModel)/Uncertainty)   # S/N for den flade model
     
     return SN_m, SN_f
