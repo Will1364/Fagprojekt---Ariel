fd9da67043449495e2ae612ea15f888cc534a21b
Will1364 <97221309+Will1364@users.noreply.github.com>
2023-06-15 10:23:11 +0200

    Update binData.py

--- binData.py
+++ binData.py
@@ -1,18 +1,17 @@
 """
 Created on Mon Mar  6 09:54:13 2023
-
-@author: William
 """
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 ###################################################################################################################################
-# Formålet med denne funktion er at binne et datasæt og derved reducere støj
-# I funktionen insættes listen med målepunkternes bølgelængder (x-koordinaterne), samt målepunkternes y-koordinat og 
+# Formålet med denne funktion er at binne et datasæt og derved reducere støj.
+# Metoden der bliver brugt er invers varians vægtning.
+# I funktionen insættes listen med målepunkternes bølgelængder (x-koordinaterne), samt målepunkternes y-koordinat og støj i form af en standardafvigelse.
 
 def binData(Wavelength, Signal, Noise,Resolution):
     
-    bins = [Wavelength[0]] # definerer en liste med grænser mellem bins
+    bins = [Wavelength[0]] # definerer en liste med grænser mellem bins.
     
     
     # Det første loop i koden bruges til at definere kasser/bins, som dataen skal sorteres efter
@@ -33,26 +32,22 @@ def binData(Wavelength, Signal, Noise,Resolution):
     x_avg = np.array([])
     y_avg_error = np.array([])
     
-    # Det næste loop bruges til at indele datasættet i de binintervaller, som blev defineret i første loop
-    # Gennemsnittet af de datapunkter, som falder inden for hvert bin udregnes, og disse værdier gemmes som vores nye datasæt
     Wavelength = np.array(Wavelength)
     Signal = np.array(Signal)
     
-    # Det næste loop bruges til at indele datasættet i de binintervaller, som blev defineret i første loop
-    # Gennemsnittet af de datapunkter, som falder inden for hvert bin udregnes, og disse værdier gemmes som vores nye datasæt
     
     for i in range(max(Xbinned)): # Dette loop binner datasættet et bin af gangen
         currentBin = np.zeros(len(Wavelength))
         currentBin[Xbinned == i+1] = Wavelength[Xbinned == i+1] # Bølgelængder, der ligger inden for pågældende bin gemmes i liste
        
+        # datapunkterne inden for pågældende bin vægtes og ligges sammen til en form for vægtet gennemsnit
         RS = 1/(Noise[Wavelength == currentBin]**2)
-       
-        binnedDatapoints = np.sum(Signal[Wavelength == currentBin]*RS)/np.sum(RS) # Signalet tilhørende binnets  bølgelængde interval gemmes i liste
+        binnedDatapoints = np.sum(Signal[Wavelength == currentBin]*RS)/np.sum(RS)
         
         
-        y_avg = np.append(y_avg,binnedDatapoints) # gennemsnittet af signaler i pågældende bin udregnes og gemmes som nyt datapunkt
-        x_avg = np.append(x_avg,np.mean(currentBin[currentBin > 0])) # midten af binnet gemmes som bølgelængden tilhørende nyt datapunkt 
-        y_avg_error = np.append(y_avg_error,1/np.sqrt(np.sum(RS)))
+        y_avg = np.append(y_avg,binnedDatapoints) # De binnede datapunkter gemmes i et array
+        x_avg = np.append(x_avg,np.mean(currentBin[currentBin > 0])) # midten af hvert bin gemmes som bølgelængden tilhørende nyt datapunkt 
+        y_avg_error = np.append(y_avg_error,1/np.sqrt(np.sum(RS))) #tilhørende støj gemmes i et array.
         
     return y_avg, x_avg, y_avg_error
 
